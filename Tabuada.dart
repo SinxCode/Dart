9a7472248a4a2bb646e52a30ebf2c3678a21b61b
GabrielJBrito <gabriel.jbrito2@gmail.com>
2023-10-01 13:03:47 -0300

    TABUADA COM INPUT

--- Tabuada.dart
+++ Tabuada.dart
@@ -1,14 +1,26 @@
-
+import 'dart:io';
 void main (){
 
-int Tabuada = 5;
-int total = 1;
-int contador = 1;
+print('Informe um número: ');
+String? n = stdin.readLineSync(); // Todo input será uma string, e é necessário o '?' pois ele diz que a variável que guardará o input poderá receber um valor nulo
+if (n != null) { //Pegando somente dado diferente de nulo
+  int tabuada = int.parse(n); //Convertendo valor digitado dentro de outra variável
+  print('O número informado foi: $tabuada');
+
+  int total = 1;
+  int contador = 1;
 
 for(contador; contador < 10 + 1; contador = contador +1 ){
-total = (Tabuada * contador);
-print('$Tabuada * $contador = $total');
+total = (tabuada * contador);
+print('$tabuada * $contador = $total');
 
 }
 
 }
+
+
+
+
+
+}
+ 
