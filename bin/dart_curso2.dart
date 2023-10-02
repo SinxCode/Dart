58f620cf8ea165da74166994c834e8aeb6148245
Sinx <gabriel.jbrito2@gmail.com>
2023-10-02 10:16:20 -0300

    Criando objetos

--- bin/dart_curso2.dart
+++ bin/dart_curso2.dart
@@ -6,16 +6,14 @@ String nome = 'Laranja';
 double peso = 100.2;
 String cor = 'Verde e Amarela';
 String sabor = 'Doce e cítrica';
-int diasDesdeColheita = 40;
+int diasDesdeColheita = 30;
 bool isMadura;
 //--------------------------------
-
-if (diasDesdeColheita >=30) {
+ (diasDesdeColheita >=30) {
 isMadura = true;
 }
 else{
 isMadura = false;
 }
-
 
 }
